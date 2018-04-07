cb4997719c684248779b7e4b3b956e4c03ff6c98
Simon Christmann <simon@christmann.email>
2018-04-07 16:36:28 +0200

    Fix Testing

--- test.js
+++ test.js
@@ -1,14 +1,16 @@
-const Timer = require('Timer.js');
+const Timer = require('./Timer.js');
+
+console.log('Testing YaTL');
 
 var timer1 = new Timer(()=>{
-	console.log("every 2 Seconds");
+	console.log("Timer1 - every 2 Seconds");
 },2000).start();
 
 var timer2 = new Timer(()=>{
-	console.log("every 3 Seconds");
+	console.log("Tiemr2 - every 3 Seconds");
 },3000).start();
 
 var timer3 = new Timer(()=>{
-	console.log("reset timer1 every 5sec");
+	console.log("Timer3 - reset timer1 every 5sec");
 	timer1.reset(1000);
-},2000).start();
+},5000).start();
